fdcf135f15d5a7209f74d51bab0e76d9cc135d13
jcbcouch <jcbcouch@gmail.com>
2025-10-16 15:09:05 -0500

    Seed Menu Items

--- FusionMarket_api/Data/ApplicationDbContext.cs
+++ FusionMarket_api/Data/ApplicationDbContext.cs
@@ -13,5 +13,103 @@ namespace FusionMarket_api.Data
 
         }
 
+        public DbSet<MenuItem> MenuItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<MenuItem>().HasData(new MenuItem
+            {
+                Id = 1,
+                Name = "Spring Roll",
+                Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                Image = "",
+                Price = 7.99,
+                Category = "Appetizer",
+                SpecialTag = ""
+            },
+            new MenuItem
+            {
+                Id = 2,
+                Name = "Samosa",
+                Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                Image = "",
+                Price = 8.99,
+                Category = "Appetizer",
+                SpecialTag = ""
+            },
+            new MenuItem
+            {
+                Id = 3,
+                Name = "Soup",
+                Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                Image = "",
+                Price = 8.99,
+                Category = "Appetizer",
+                SpecialTag = "Best Seller"
+            },
+            new MenuItem
+            {
+                Id = 4,
+                Name = "Noodles",
+                Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                Image = "",
+                Price = 10.99,
+                Category = "Entrée",
+                SpecialTag = ""
+            },
+            new MenuItem
+            {
+                Id = 5,
+                Name = "Pav Bhaji",
+                Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                Image = "",
+                Price = 12.99,
+                Category = "Entrée",
+                SpecialTag = "Top Rated"
+            },
+            new MenuItem
+            {
+                Id = 6,
+                Name = "Paneer Pizza",
+                Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                Image = "",
+                Price = 11.99,
+                Category = "Entrée",
+                SpecialTag = ""
+            },
+            new MenuItem
+            {
+                Id = 7,
+                Name = "Mango Paradise",
+                Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                Image = "",
+                Price = 13.99,
+                Category = "Dessert",
+                SpecialTag = "Chef's Special"
+            },
+            new MenuItem
+            {
+                Id = 8,
+                Name = "Carrot Love",
+                Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                Image = "",
+                Price = 4.99,
+                Category = "Dessert",
+                SpecialTag = ""
+            },
+            new MenuItem
+            {
+                Id = 9,
+                Name = "Sweet Rolls",
+                Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                Image = "",
+                Price = 4.99,
+                Category = "Dessert",
+                SpecialTag = "Chef's Special"
+            });
+        }
+
     }
 }
---
+++ FusionMarket_api/Migrations/20251016200655_addMenuItemToDbAndSeed.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FusionMarket_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class addMenuItemToDbAndSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MenuItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SpecialTag = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuItems", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "MenuItems",
+                columns: new[] { "Id", "Category", "Description", "Image", "Name", "Price", "SpecialTag" },
+                values: new object[,]
+                {
+                    { 1, "Appetizer", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "", "Spring Roll", 7.9900000000000002, "" },
+                    { 2, "Appetizer", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "", "Samosa", 8.9900000000000002, "" },
+                    { 3, "Appetizer", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "", "Soup", 8.9900000000000002, "Best Seller" },
+                    { 4, "Entrée", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "", "Noodles", 10.99, "" },
+                    { 5, "Entrée", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "", "Pav Bhaji", 12.99, "Top Rated" },
+                    { 6, "Entrée", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "", "Paneer Pizza", 11.99, "" },
+                    { 7, "Dessert", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "", "Mango Paradise", 13.99, "Chef's Special" },
+                    { 8, "Dessert", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "", "Carrot Love", 4.9900000000000002, "" },
+                    { 9, "Dessert", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "", "Sweet Rolls", 4.9900000000000002, "Chef's Special" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MenuItems");
+        }
+    }
+}
