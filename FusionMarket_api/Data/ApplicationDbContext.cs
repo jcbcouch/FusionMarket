6a670c70fbafad2f40e1204f0e89944dca6b674f
jcbcouch <jcbcouch@gmail.com>
2025-10-16 16:47:13 -0500

    Add Image for Menu Item

--- FusionMarket_api/Data/ApplicationDbContext.cs
+++ FusionMarket_api/Data/ApplicationDbContext.cs
@@ -24,7 +24,7 @@ namespace FusionMarket_api.Data
                 Id = 1,
                 Name = "Spring Roll",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "",
+                Image = "images/spring_roll.jpg",
                 Price = 7.99,
                 Category = "Appetizer",
                 SpecialTag = ""
@@ -34,7 +34,7 @@ namespace FusionMarket_api.Data
                 Id = 2,
                 Name = "Samosa",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "",
+                Image = "images/samosa.jpg",
                 Price = 8.99,
                 Category = "Appetizer",
                 SpecialTag = ""
@@ -44,7 +44,7 @@ namespace FusionMarket_api.Data
                 Id = 3,
                 Name = "Soup",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "",
+                Image = "images/soup.jpg",
                 Price = 8.99,
                 Category = "Appetizer",
                 SpecialTag = "Best Seller"
@@ -54,7 +54,7 @@ namespace FusionMarket_api.Data
                 Id = 4,
                 Name = "Noodles",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "",
+                Image = "images/noodles.jpg",
                 Price = 10.99,
                 Category = "Entrée",
                 SpecialTag = ""
@@ -64,7 +64,7 @@ namespace FusionMarket_api.Data
                 Id = 5,
                 Name = "Pav Bhaji",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "",
+                Image = "images/pav_bhaji.jpg",
                 Price = 12.99,
                 Category = "Entrée",
                 SpecialTag = "Top Rated"
@@ -74,7 +74,7 @@ namespace FusionMarket_api.Data
                 Id = 6,
                 Name = "Paneer Pizza",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "",
+                Image = "images/pizza.jpg",
                 Price = 11.99,
                 Category = "Entrée",
                 SpecialTag = ""
@@ -84,7 +84,7 @@ namespace FusionMarket_api.Data
                 Id = 7,
                 Name = "Mango Paradise",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "",
+                Image = "images/mango_paradise.jpg",
                 Price = 13.99,
                 Category = "Dessert",
                 SpecialTag = "Chef's Special"
@@ -94,7 +94,7 @@ namespace FusionMarket_api.Data
                 Id = 8,
                 Name = "Carrot Love",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "",
+                Image = "images/carrot_love.jpg",
                 Price = 4.99,
                 Category = "Dessert",
                 SpecialTag = ""
@@ -104,12 +104,11 @@ namespace FusionMarket_api.Data
                 Id = 9,
                 Name = "Sweet Rolls",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "",
+                Image = "images/sweet_rolls.jpg",
                 Price = 4.99,
                 Category = "Dessert",
                 SpecialTag = "Chef's Special"
             });
         }
-
     }
 }
---
+++ FusionMarket_api/Migrations/20251016214509_UpdateSeedMenuItems.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FusionMarket_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSeedMenuItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Image",
+                value: "images/spring_roll.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Image",
+                value: "images/samosa.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Image",
+                value: "images/soup.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Image",
+                value: "images/noodles.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Image",
+                value: "images/pav_bhaji.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Image",
+                value: "images/pizza.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Image",
+                value: "images/mango_paradise.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Image",
+                value: "images/carrot_love.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Image",
+                value: "images/sweet_rolls.jpg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Image",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Image",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Image",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Image",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Image",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Image",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Image",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Image",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Image",
+                value: "");
+        }
+    }
+}
