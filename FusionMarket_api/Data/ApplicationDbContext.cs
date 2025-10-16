fe182f5cc5dc9ce5de81418760c9147dbf33f4b5
jcbcouch <jcbcouch@gmail.com>
2025-10-16 14:47:03 -0500

    Connection String and ApplicationDbContext

---
+++ FusionMarket_api/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FusionMarket_api.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
