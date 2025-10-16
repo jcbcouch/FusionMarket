486b0178d99354ec57c03921b222c3b27c0e9e24
jcbcouch <jcbcouch@gmail.com>
2025-10-16 15:12:44 -0500

    Get All Menu Items

---
+++ FusionMarket_api/Controllers/MenuItemController.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using FusionMarket_api.Data;
+using FusionMarket_api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FusionMarket_api.Controllers
+{
+    [Route("api/MenuItem")]
+    [ApiController]
+    public class MenuItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly ApiResponse _response;
+        public MenuItemController(ApplicationDbContext db)
+        {
+            _db = db;
+            _response = new ApiResponse();
+        }
+
+        [HttpGet]
+        public IActionResult GetMenuItems()
+        {
+            _response.Result = _db.MenuItems.ToList();
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+    }
+}
