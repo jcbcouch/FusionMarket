a6e6d68a669e0a4ed8e5a3d3113880457b5f9053
jcbcouch <jcbcouch@gmail.com>
2025-10-16 16:57:44 -0500

    Add Auth Test Controller

---
+++ FusionMarket_api/Controllers/AuthTestController.cs
@@ -0,0 +1,27 @@
+﻿using FusionMarket_api.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FusionMarket_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthTestController : Controller
+    {
+        [HttpGet]
+        [Authorize]
+        public ActionResult<string> GetSomething()
+        {
+            return "You are authorized user";
+        }
+
+        [HttpGet("{someValue:int}")]
+        [Authorize(Roles = SD.Role_Admin)]
+        public ActionResult<string> GetSomething(int someValue)
+        {
+            return "You are authorized user, with Role of Admin";
+        }
+
+
+    }
+}
