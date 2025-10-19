40d8ab4959fb120f271e8d23a5d987d590559424
jcbcouch <jcbcouch@gmail.com>
2025-10-19 15:45:55 -0500

    Order Header and Order Details Table

--- FusionMarket_api/Data/ApplicationDbContext.cs
+++ FusionMarket_api/Data/ApplicationDbContext.cs
@@ -14,6 +14,9 @@ namespace FusionMarket_api.Data
         }
 
         public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
