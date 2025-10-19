4ba50df1cc89d91d5683f157e0a9af22702c98eb
jcbcouch <jcbcouch@gmail.com>
2025-10-19 15:22:05 -0500

    Register Endpoint

--- FusionMarket_api/Models/Dto/RegisterRequestDTO.cs
+++ FusionMarket_api/Models/Dto/RegisterRequestDTO.cs
@@ -9,7 +9,7 @@ namespace FusionMarket_api.Models.Dto
         [Required]
         public string Password { get; set; } = string.Empty;
         [Required]
-        public string ConfirmPassword { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
     }
 }
