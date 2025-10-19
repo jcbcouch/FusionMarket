84b8173e007dc1ecdc58a7114baf5aef11d55674
jcbcouch <jcbcouch@gmail.com>
2025-10-19 17:37:44 -0500

    Update Order

--- FusionMarket_api/Controllers/OrderController.cs
+++ FusionMarket_api/Controllers/OrderController.cs
@@ -123,5 +123,77 @@ namespace FusionMarket_api.Controllers
                 return StatusCode((int)HttpStatusCode.InternalServerError, _response);
             }
         }
+
+        [HttpPut("{orderId:int}")]
+        public ActionResult<ApiResponse> UpdateOrder(int orderId, [FromBody] OrderHeaderUpdateDTO orderHeaderDTO)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    OrderHeader? orderHeaderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.OrderHeaderId == orderId);
+
+                    if (orderHeaderFromDb == null)
+                    {
+                        _response.IsSuccess = false;
+                        _response.StatusCode = HttpStatusCode.NotFound;
+                        _response.ErrorMessages.Add("Order not found");
+                        return NotFound(_response);
+                    }
+
+                    if (!string.IsNullOrEmpty(orderHeaderDTO.PickUpName))
+                    {
+                        orderHeaderFromDb.PickUpName = orderHeaderDTO.PickUpName;
+                    }
+                    if (!string.IsNullOrEmpty(orderHeaderDTO.PickUpPhoneNumber))
+                    {
+                        orderHeaderFromDb.PickUpPhoneNumber = orderHeaderDTO.PickUpPhoneNumber;
+                    }
+                    if (!string.IsNullOrEmpty(orderHeaderDTO.PickUpEmail))
+                    {
+                        orderHeaderFromDb.PickUpEmail = orderHeaderDTO.PickUpEmail;
+                    }
+                    if (!string.IsNullOrEmpty(orderHeaderDTO.Status))
+                    {
+
+                        if (orderHeaderFromDb.Status.Equals(SD.status_confirmed, StringComparison.InvariantCultureIgnoreCase)
+                            && orderHeaderDTO.Status.Equals(SD.status_readyForPickUp, StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            orderHeaderFromDb.Status = SD.status_readyForPickUp;
+                        }
+
+                        if (orderHeaderFromDb.Status.Equals(SD.status_readyForPickUp, StringComparison.InvariantCultureIgnoreCase)
+                            && orderHeaderDTO.Status.Equals(SD.status_Completed, StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            orderHeaderFromDb.Status = SD.status_Completed;
+                        }
+                        if (orderHeaderDTO.Status.Equals(SD.status_Cancelled, StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            orderHeaderFromDb.Status = SD.status_Cancelled;
+                        }
+                    }
+
+                    _db.SaveChanges();
+
+                    _response.StatusCode = HttpStatusCode.NoContent;
+                    return Ok(_response);
+
+                }
+                else
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages = ModelState.Values.SelectMany(u => u.Errors).Select(u => u.ErrorMessage).ToList();
+                    return BadRequest(_response);
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages.Add(ex.Message);
+                return StatusCode((int)HttpStatusCode.InternalServerError, _response);
+            }
+        }
     }
 }
